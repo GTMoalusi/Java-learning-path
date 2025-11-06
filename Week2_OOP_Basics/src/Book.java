f345e983dafedb72d7ac32fd5a315ace37f9b8ee
Thabang Moalusi <thabang.moalusi@yahoo.com>
2025-11-06 13:47:34 +0200

    Basic Java OOP Book-Class

---
+++ Week2_OOP_Basics/src/Book.java
@@ -0,0 +1,27 @@
+public class Book {
+
+    String title;
+    String author;
+    int price;
+
+    //Constructor
+    Book(String title, String author, int price){
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+    Book(String title, String author){
+        this.title = title;
+        this.author = author;
+        this.price = 0;
+    }
+    Book(String title){
+        this.title = title;
+        this.author = "Unknown Author";
+        this.price = 0;
+    }
+
+    public String toString(){
+        return "Title: " + title + "\nAuthor: " + author + "\nPrice: R" + price + "\n";
+    }
+}
