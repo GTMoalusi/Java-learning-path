b17a7d1e5fec34e749a1c25c56b65733816c7075
Thabang Moalusi <thabang.moalusi@yahoo.com>
2025-12-06 21:53:46 +0200

    Circle Area and Perimeter

---
+++ UdemyCourse/src/Arithmetics/circle_Area_Perimeter.java
@@ -0,0 +1,26 @@
+package Arithmetics;
+
+import java.util.Scanner;
+
+public class circle_Area_Perimeter {
+    public static void main(String[] args) {
+        //Write a Java program to print the area and perimeter of a circle.
+        //Test Data:
+        //Radius = 7.5
+        //Expected Output
+        //Perimeter is = 47.12388980384689
+        //Area is = 176.71458676442586
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your circle radius: ");
+        double radius = scanner.nextDouble();
+        scanner.nextLine();
+
+        System.out.println();
+        double circlePerimeter = 2 * Math.PI * radius;
+        System.out.println("Perimeter is = " + circlePerimeter);
+        double circleArea = Math.PI * Math.pow(radius, 2);
+        System.out.println("Area is = " + circleArea);
+    }
+}
