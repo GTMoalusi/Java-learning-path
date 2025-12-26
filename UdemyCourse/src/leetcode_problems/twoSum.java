f79852704b07670043097f3dfdede097a64471d6
thabangmoalusi <thabang.moalusi@yahoo.com>
2025-12-26 19:13:18 +0200

    two sum leetcode challenge, not yet done...

---
+++ UdemyCourse/src/leetcode_problems/twoSum.java
@@ -0,0 +1,33 @@
+package leetcode_problems;
+
+public class twoSum {
+
+    public static int[] twoSum(int[] nums, int target){
+
+        return nums;
+    }
+
+    public static void main(String[] args) {
+        /* Leetcode Challenge easy-mode
+        * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+        * You many assume that each input would have exactly one solution, and you may not use the same element twice.
+        * You can return the answer in any order.
+        *
+        * Example1:
+        * Input: nums = [2,7,11,15], target = 9
+        * Output: [0,1]
+        * Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+        *
+        * Example2:
+        * Input: nums = [3,2,4], target = 6
+        * Output: [1,2]
+        *
+        * Example3:
+        * Input: nums = [3,3], target = 6
+        * Output: [0,1]
+        *
+        * NB: Only one valid answer exists.
+        * Follow-up: Can you come up with an algorithm that is less than 0(n(square)) time complexity?
+        * */
+    }
+}
